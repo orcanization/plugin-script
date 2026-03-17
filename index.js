b093e0e6246787166234eb11453e921a7ed9f758
marian <elias-marian@web.de>
2026-03-17 20:40:51 +0100

    Refactor package info retrieval in build script

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 import {mkdir, writeFile} from "node:fs/promises";
-import {existsSync} from "node:fs";
-import path from "node:path";
+import {existsSync, readFileSync} from "node:fs";
+import {join} from "node:path";
 import * as esbuild from "esbuild";
-import packageInfo from "./package.json" with {type: "json"};
 import {uiToStringPlugin} from "./esbuild-plugins/plugin-ui-string.js";
 
 const devRoot = process.cwd()
 
+const pluginPackageInfoRaw = readFileSync(join(devRoot, "package.json"), "utf8")
+const pluginPackageInfo = JSON.parse(pluginPackageInfoRaw)
+
 const defaultExternals = ["react", "react-dom", "next"];
-const userExternals = packageInfo.pluginConfig?.external || [];
+const userExternals = pluginPackageInfo["pluginConfig"]?.external || [];
 
 const res = await esbuild.build({
     entryPoints: ["src/index.ts"],
@@ -23,9 +25,9 @@ const res = await esbuild.build({
 
 const code = res.outputFiles[0].text;
 
-const distFolder = path.join(devRoot, ".dist");
-if (!existsSync(distFolder)) await mkdir(distFolder, { recursive: true });
-const outPath = path.join(distFolder, `${packageInfo.name}.js`);
+const distFolder = join(devRoot, ".dist");
+if (!existsSync(distFolder)) await mkdir(distFolder, {recursive: true});
+const outPath = join(distFolder, `${pluginPackageInfo.name}.js`);
 await writeFile(outPath, code);
 
 console.log(`✅ Plugin bundled successfully: ${outPath}`);
