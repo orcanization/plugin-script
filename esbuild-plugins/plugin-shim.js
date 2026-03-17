6526582c70b10ad9550ac8e3c9cce9999e26c4ff
marian <elias-marian@web.de>
2026-03-17 20:22:51 +0100

    Fix file path

--- esbuild-plugins/plugin-shim.js
+++ esbuild-plugins/plugin-shim.js
@@ -1,4 +1,5 @@
 import {readFileSync} from "node:fs";
+import {join} from "node:path";
 
 export const reactShimPlugin = {
     name: 'react-shim',
@@ -17,15 +18,17 @@ export const reactShimPlugin = {
 
         build.onLoad({filter: /.*/, namespace: 'shim'}, (args) => {
             if (args.path === 'react-shim') {
+                const filePath = join(__dirname, "shims", "react-shim.js");
                 return {
-                    contents: readFileSync('shims/react-shim.js', 'utf8'),
+                    contents: readFileSync(filePath, 'utf8'),
                     loader: 'js',
                 };
             }
 
             if (args.path === 'jsx-runtime-shim') {
+                const filePath = join(__dirname, "shims", "jsx-runtime-shim.js");
                 return {
-                    contents: readFileSync('shims/jsx-runtime-shim.js', 'utf8'),
+                    contents: readFileSync(filePath, 'utf8'),
                     loader: 'js',
                 };
             }
